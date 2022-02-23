3d6f8504bff49dc9cc6c871ca8dd8809114ceef6
Ruslan Karachun <free.changer@gmail.com>
2022-02-23 15:42:59 +0300

    Added function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ function double(x) {
     return x * 2
 }
 
+function* removeRandomElements(a) {
+    const l = a.length
+    if (l !== 0 || l === 1) {
+        yield a
+    }
+
+    for (let i = 0; i < l; i++) { // i - index of elements to delete
+        const aCopy = [...a]
+        delete aCopy[i]
+        const newA = aCopy.filter((x) => x)
+        if (newA.length !== 0) {
+            yield newA
+        }
+        for (let y of removeRandomElements(newA)) {
+            yield y
+        }
+    }
+}
+
 module.exports = {
-    double
+    double,
+    removeRandomElements
 }
